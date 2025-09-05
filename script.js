c3855998c605aad857fae3190ca6dfab53a831db
Cristian <cristian_campigot>
2025-09-04 22:26:00 -0300

    aula javascript

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-alert("Olá mundo")
+// alert("Olá mundo")
+// alert("oi")
+// alert("tudo bem?")
+// alert("Você vem sempre aqui?")
 
+// let idade
 
+// idade = 18
+// alert(idade)
+
+// idade = "dezoito"
+// alert(idade) 
+
+// idade = "volte ao inicio"
+// alert(idade)
+
+const amor = "Meu primeiro amor SZ"
+alert(amor)
+alert(amor)
+let = amor2 = "Meus segundo amor "
+
+var antigao
