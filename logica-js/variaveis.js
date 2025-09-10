7c026f6b9774a0da8a98d8913da3057f7cf6d732
Cristian <cristian_campigot>
2025-09-09 22:14:45 -0300

    variaveis em Javascript

---
+++ logica-js/variaveis.js
@@ -0,0 +1,26 @@
+//variavel com var
+//var nome = "cristian" // string //(significa um conjunto de caracteres)
+// var nome = "joao"
+//nome = "joao"
+ 
+//variavel com let
+// let nome = "Cristian"
+// nome = "João"
+
+// variavel declarada com const
+const nome = "Cristian" //string
+const sobrenome = "Campigotto"
+const idade = 28 // number
+const masculino = true // boolean
+
+// array
+const fruta = ["laranja","maçã","banana"]
+
+console.log(fruta[2])
+
+
+// console.log("Meu nome é "+ nome + " "+ sobrenome + " e minha idade é " + idade + " anos")
+
+
+//console.log(`Meu nome é ${nome} ${Campigotto} e minha idade é ${28} anos`)
+
