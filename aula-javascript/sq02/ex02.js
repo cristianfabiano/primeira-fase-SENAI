fa15371bc647f2a58bdcde12cdb60eb6a45c4174
Cristian <cristian_campigot>
2025-09-10 22:21:02 -0300

    ex02 javascript

---
+++ aula-javascript/sq02/ex02.js
@@ -0,0 +1,16 @@
+let numero
+numero = Number(prompt("Digite o número"))
+if (numero % 2 == 0){
+alert("par")
+}else{
+alert("ímpar")
+}
+
+
+
+//ler um numero
+
+//se o resto da divisao por 2 for 0
+//informar que é par 
+//senão
+//informar que é ímpar
