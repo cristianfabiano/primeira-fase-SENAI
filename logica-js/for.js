6b66e82d456c328e7cc749b4c6d87cb8d871efba
Cristian <cristian_campigot>
2025-09-30 22:21:21 -0300

    funcoes & for

---
+++ logica-js/for.js
@@ -0,0 +1,35 @@
+// for(let i = 0; i<=10; i++){
+//     console.log(i)
+
+// }
+
+// for(let i =10; i>=0; i--){
+//     console.log(i)
+
+// }
+
+// for(let i =0; i<=20; i+=2){
+//     console.log(i)
+// }
+// let soma = 0
+// for(let i = 1; i<100; i++){
+//     soma+=i
+// }
+// console.log("Resultado da soma ", soma)
+
+
+
+// let alunos = ["joao", "maria", "joana"]
+// alunos.push("pedro")
+//         for(let i =0; i<alunos.length; i ++){
+//             console.log("aluno: ", alunos[i])
+//         }
+
+// console.log(alunos.length) 
+
+
+
+// let frutas = ["manga", "abacaxi", "laranja", "uva", "pessego"]
+// for(let i =0; i<frutas.length; i++){
+//     console.log("fruta: ", frutas[i])
+// }
---
+++ logica-js/funcoes.js
@@ -0,0 +1,25 @@
+// function saudacao(nome){
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// const saudacao = (nome) =>{
+//     console.log(`Bom dia, ${nome}`)
+// }
+
+// saudacao("Cristian")
+// saudacao("Maria")
+
+// const soma =(x,y) => x+y
+
+// const soma = (a,b) =>{
+//     return a+b
+// 
+const subtracao = (x,y) => x-y
+const soma = (x,y) => x+y
+const multiplicacao = (x,y) => x*y
+const divisao = (x,y) => x/y
+
+return x+y
+
+console.log(`a soma é ${soma(10,20)}`)
+
