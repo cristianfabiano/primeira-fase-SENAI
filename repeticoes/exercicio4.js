2759c3f820140a8df011f696a09d70c5b7d403be
Cristian <cristian_campigot>
2025-10-06 22:07:50 -0300

    exercicios repeticoes

---
+++ repeticoes/exercicio4.js
@@ -0,0 +1,5 @@
+
+for(let i=1; i<=10; i++){
+    let resultado = i * 5
+    console.log(i + "X" + 5 + "=" + resultado);
+}
--- repeticoes/exercico4.js
+++
@@ -1,8 +0,0 @@
-const numero = 5;
-const tabuada = document.getElementById('tabuada');
-for(let i =1; i<=10; i++){
-    const resultado = numero * i;
-    const linha = document.createElement('p');
-    linha.textContent = `${numero} x ${i} = ${resultado}`;
-    tabuada.appendChild(linha)
-}
