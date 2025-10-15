12cc84e92a247a21a8d1b4125dbda1716ee30e36
Cristian <cristian_campigot>
2025-10-14 22:18:38 -0300

    DOM/Selecao

---
+++ logica-js/setTimeout.js
@@ -0,0 +1,14 @@
+ // setTimeout(()=>{
+//     console.log("Executei depois de 3 seg")
+// },3000)
+// function saudacao(){
+//     console.log("olá")
+// }
+// function imprimeDado(){
+//     console.log("imprimir")
+// // }
+
+ const soma = ()=> console.log(5+2)
+
+setTimeout(soma,5000)
+setTimeout(soma,300)
