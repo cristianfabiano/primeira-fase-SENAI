fbba1498d4f2a0740ce8a50db7c840b5a55a4712
Cristian <cristian_campigot>
2025-10-16 21:57:49 -0300

    for

---
+++ logica-js/for.js
@@ -0,0 +1,22 @@
+
+
+// for(let i =1; i <=5; i++){
+//     console.log("Numero com for:", i);
+// }
+// usando o Loop for, printe na tela os 100 primeiros números
+
+// for(let i =0; i <100; i++){
+//     console.log("Numero com for:", i);
+// }cd
+// let dia = 1
+// switch (dia) {
+// case 1:
+//     console.log("Segundou");
+//     break;
+// case 2:
+//     console.log("Sextou");
+//     break;
+// default:
+//     console.log("Opção Inválida");
+//     break;
+// }
