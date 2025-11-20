92baf26467859bec37892487e8fb6e64dc85241a
cristianfabiano <cristian.fabiano69@gmail.com>
2025-11-19 21:02:45 -0300

    Corrigido: liftsenai agora é pasta normal

---
+++ logica-js/DOM/selecao/js/script.js
@@ -0,0 +1,11 @@
+//buscar
+
+const inputForm = document.querySelector("#buscar")
+
+const botaoBuscar = document.querySelector("#botaoBuscar")
+
+const resultadoDiv = document.querySelector("#resultado")
+
+inputForm.addEventListener("keydown" ,(e) => {
+    console.log(e.key)
+})
